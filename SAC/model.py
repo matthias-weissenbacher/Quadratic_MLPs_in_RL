e28b9cd44f3ee3eaec7d21ff5e4471b58fdb474e
Matthias Weissenbacher <77483344+matthias-weissenbacher@users.noreply.github.com>
2021-04-26 14:26:41 +0900

    Update model.py

--- SAC/model.py
+++ SAC/model.py
@@ -152,39 +152,3 @@ class DeterministicPolicy(nn.Module):
         return super(DeterministicPolicy, self).to(device)
 
 
-class Sys_R(nn.Module):
-	def __init__(self,state_dim, action_dim, fc1_units, fc2_units):
-		super(Sys_R, self).__init__()
-
-		# Q1 architecture
-		self.l1 = nn.Linear(2 * state_dim + action_dim,fc1_units)
-		self.l2 = nn.Linear(fc1_units, fc2_units)
-		self.l3 = nn.Linear(fc2_units, 1)
-		self.apply(weights_init_)
-
-
-	def forward(self, state,next_state, action):
-		sa = torch.cat([state,next_state, action], 1)
-
-		q1 = F.relu(self.l1(sa))
-		q1 = F.relu(self.l2(q1))
-		q1 = self.l3(q1)
-		return q1
-
-
-class SysModel(nn.Module):
-	def __init__(self, state_size, action_size, fc1_units, fc2_units):
-		super(SysModel, self).__init__()
-		self.l1 = nn.Linear(state_size + action_size, fc1_units)
-		self.l2 = nn.Linear(fc1_units, fc2_units)
-		self.l3 = nn.Linear(fc2_units, state_size)
-		self.apply(weights_init_)
-
-	def forward(self, state, action):
-		"""Build a system model to predict the next state at a given state."""
-		xa = torch.cat([state, action], 1)
-
-		x1 = F.relu(self.l1(xa))
-		x1 = F.relu(self.l2(x1))
-		x1 = self.l3(x1)
-		return x1
